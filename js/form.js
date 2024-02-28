aa980d6a1c77c538fafc060927a2155eaaff2cd8
Disturbless <seris.koerala@gmail.com>
2024-02-28 20:43:41 +0545

    my first comit after making search bar

---
+++ js/form.js
@@ -0,0 +1,14 @@
+// // toggle hide / show password
+// const eyeDom = document.querySelector('.wrap__togglePassword');
+// const passwordInput = document.querySelector('#password');
+
+// eyeDom.addEventListener('click', () => {
+//     const inputType = passwordInput.attributes['type'].value === 'password' ? 'text' : 'password'
+//     passwordInput.setAttribute('type', inputType)
+// })
+
+// // terms enable / disable button 
+// const termsCheckbox = document.querySelector('#terms')
+// const registerButton = document.querySelector('.wrap__form__register');
+
+// termsCheckbox.addEventListener('click', () => registerButton.disabled = !termsCheckbox.checked)
